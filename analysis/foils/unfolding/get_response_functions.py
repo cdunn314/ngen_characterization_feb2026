5cf45f652441f612dba7ef60119efcf93f229ffd
Collin Dunn <cdunn314@mit.edu>
2026-04-30 13:55:42 -0400

    Initial unfolding commit

---
+++ analysis/foils/unfolding/get_response_functions.py
@@ -0,0 +1,52 @@
+""" use NJOY and sandy to get response functions and uncertainties for 
+specified reactions in a specified nuclear data library. 
+note some libraries may be missing reactions and/or covariances.
+will also output the used energy group structure - together these
+can be used for unfolding
+"""
+import numpy as np
+from nfoils.reaction import PostprocessReactions
+
+# set predefined sandy energy group structure
+# make sure to import sandy.energy_grids if using this 
+# see sandy repo for details: https://github.com/luca-fiorito-11/sandy
+import sandy.energy_grids
+ek=sandy.energy_grids.SCALE238
+
+# or define your own group structure elsewhere and import it in
+# ek=np.fromfile('group_structure_20.txt', sep=" ")
+
+# path to local nuclear data library.
+# the relevant files must be downloaded from
+# https://nds.iaea.org/public/download-endf/
+library = '../../../../../cross_sections/IRDFF-II_ENDF/' 
+
+# set path to a json with foil reaction data
+# should include dictionaries for each reaction including:
+#  MAT ENDF6 material number of parent isotope e.g. 491150
+#  MT ENDF6 reaction number e.g. 102
+#  density of the foil in g/cm3
+#  mass of the foil in g
+#  atomic mass of the foil in amu
+#  abundance of the parent isotope in the foil
+data_file_name = 'reactions.json'
+
+# input list of reaction labels for plotting, as r-strings/f-strings 
+# these must match the order of the reactions listed in the foil data json
+reaction_labels = [ 
+                   "${}^{93}$Nb(n,2n)",
+                   "${}^{58}$Ni(n,2n)"]
+
+# endf nuclear data files, should be saved in library.
+# list here in the same order as reactions in reaction_file
+endf_files = [
+    'IRDFF-II.endf',
+    'IRDFF-II.endf'
+]
+
+# load group structure and set library
+reactions = PostprocessReactions(ek,library)
+
+# get response functions and/or nuclear data uncertainties
+reactions.run_rf(data_file_name,reaction_labels,endf_files)
+#reactions.run_stdev(data_file_name,reaction_labels,endf_files)
